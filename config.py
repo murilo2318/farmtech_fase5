6eb4f65e581d88339245e16d6b8f35e3b445d1d3
Murilo de Faria Benhossi <murilobenhossi@gmail.com>
2025-08-26 16:48:06 -0600

    config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+    MYSQL_USER = os.getenv("MYSQL_USER", "root")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
+    MYSQL_DB = os.getenv("MYSQL_DB", "farmtech")
